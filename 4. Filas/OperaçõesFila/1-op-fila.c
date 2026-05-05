c796cb33add5fc0325aee28ba0a859e30cffb1f7
Leonardo Schimock <dev.leonardosch@hotmail.com>
2026-05-05 00:24:48 -0300

    Update: 1-op-fila.c

--- 4. Filas/OperaçõesFila/1-op-fila.c
+++ 4. Filas/OperaçõesFila/1-op-fila.c
@@ -11,3 +11,41 @@ typedef struct {
 void iniciar(Fila *f) {
     f -> fim = -1;
 }
+
+//verificar se está vazia
+int vazia(Fila *f) {
+    return f -> fim == -1;
+}
+
+//verifica se está cheia
+int cheia(Fila *f) {
+    return f -> fim == MAX - 1;
+}
+
+//insere no final [0]
+void inserir(Fila *f, int valor) {
+    if (cheia(f)) {
+        printf("Fila Cheia!\n");
+        return;
+    }
+    f -> fim++;
+    f -> dados[f -> fim] = valor;
+    printf("Inseriu: %d\n", valor);
+}
+
+void mostrar(Fila *f) {
+    if (vazia(f)) {
+        printf("Fila Vazia!\n");
+        return;
+    }
+    printf("Fila: ");
+    for (int i = 0; i <= f -> fim; i++){
+        printf("%d", f -> dados[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    Fila fila;
+    iniciar(&fila);
+}
